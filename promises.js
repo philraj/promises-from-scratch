301001df94c3b2fbf759546ef2db551bcd2e6dcb
Phil Rajchgot <tophil@outlook.com>
2016-02-14 02:08:15 -0500

    tinkering with ideas

--- promises.js
+++ promises.js
@@ -1,25 +1,48 @@
-// var promise = new Promise(function(resolve, reject){
-//   //...
-// });
-// //if operation is successful, resolve is called
-// //otherwise, reject is called
-
 //promise constructor
-function MyPromise (executor) {
+function MyPromise (resolver) {
+  console.log('constructing promise');
+  var resolveCallback;
+
   function resolve (val) {
-
+    resolveCallback(val)
   }
-
-  function reject (err) {
-
-  }
-
-  executor(resolve, reject)
+  console.log('calling resolver')
+  resolver(resolve)
 
   this.then = function (callback) {
-    callback(val)
-  },
-  this.catch = function (callback) {
-    callback()
+    console.log('calling then');
+    resolveCallback = callback
+    return new MyPromise(callback)
   }
 }
+
+var promise = new MyPromise(function(resolve){
+  setTimeout(function(){
+    resolve("Resolved.")
+  }, 1000)
+})
+
+promise.then(function(val){
+  console.log('test');
+  console.log(val)
+})
+// var promise = new Promise( function(resolve, reject) {
+//   var random = Math.random()
+//   console.log("made random value:", random)
+//
+//   if (random > 0.5) {
+//     setTimeout( function() {
+//       resolve(random)
+//     }, 1000)
+//   } else {
+//     setTimeout( function() {
+//       reject(random)
+//     }, 1000)
+//   }
+// })
+//
+// promise.then( function (val) {
+//   console.log("resolved val:", val)
+// }).then( function (val) {
+//   console.log("???", val)
+// })
