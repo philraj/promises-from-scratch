3779a5dfe670a1eb82307f5719e804caafefc4eb
Phil Rajchgot <tophil@outlook.com>
2016-02-15 21:02:09 -0500

    rewrote many anonymous functions as ES6 arrow functions

--- promises.js
+++ promises.js
@@ -1,67 +1,70 @@
 //promise constructor
 function MyPromise (executor) {
-  var value
-  var state = 'pending'
-  var resolveCallback, rejectCallback
+  var value;
+  var state = 'pending';
+  var resolveCallback, rejectCallback;
 
   function resolve (val) {
     if (resolveCallback) {
-      resolveCallback(val)
+      resolveCallback(val);
     }
     else {
-      value = val
-      state = 'resolved'
+      value = val;
+      state = 'resolved';
     }
   }
 
   function reject (val) {
     if (rejectCallback) {
-      rejectCallback(val)
+      rejectCallback(val);
     }
     else {
-      value = val
-      state = 'rejected'
+      value = val;
+      state = 'rejected';
     }
   }
 
-  this.then = function (res, rej) {
+  this.then = function(res, rej) {
     if (state = 'pending') {
-      resolveCallback = res
-      rejectCallback = rej
+      resolveCallback = res;
+      rejectCallback = rej;
     }
     else if (state === 'resolved') {
-      res(value)
+      res(value);
     }
     else {
-      rej(value)
+      rej(value);
     }
   }
 
-  executor(resolve, reject)
+  executor(resolve, reject);
 }
 
-var promise = new MyPromise( function (resolve, reject) {
-  var val = Math.random()
+var promise = new MyPromise( function(resolve, reject) {
+  var val = Math.random();
   console.log(val);
-  
+
   if (val > 0.5) {
-    setTimeout( function() {
-      resolve("Resolved:" + val)
-    }, Math.random() * 1000)
+    setTimeout(
+      () => resolve("Resolved:" + val),
+      Math.random() * 1000);
   }
   else {
-    setTimeout( function() {
-      reject("Rejected:" + val)
-    }, Math.random() * 1000)
+    setTimeout(
+      () => reject("Rejected:" + val),
+      Math.random() * 1000);
   }
 })
 
-promise.then( function (val) {
-  setTimeout( function () {
-    console.log(val)
-  }, Math.random() * 2000)
-}, function (val) {
-  setTimeout( function () {
-    console.log(val, "...Sorry!")
-  }, Math.random() * 2000)
-})
+promise.then(
+  val => {
+    setTimeout(
+      () => console.log(val),
+      Math.random() * 2000);
+  },
+  val => {
+    setTimeout(
+      () => console.log(val),
+      Math.random() * 2000);
+  }
+);
