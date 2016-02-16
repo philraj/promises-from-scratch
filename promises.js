d947e4d9852615889c530e3d3d3c8fe962ffc960
Phil Rajchgot <tophil@outlook.com>
2016-02-15 20:24:53 -0500

    Rewrote much of the constructor to include properly keeping track of the 3 possible states, and to store the resolution/rejection value if the resolve and reject callbacks haven't yet been specified in a '.then(...)', or store the callbacks if the resollution/rejection value hasn't been produced yet

--- promises.js
+++ promises.js
@@ -1,47 +1,58 @@
 //promise constructor
 function MyPromise (executor) {
-  console.log('entering mypromise constructor!');
   var value
-  var isResolved = false
-  var isRejected = false
+  var state = 'pending'
+  var resolveCallback, rejectCallback
 
   function resolve (val) {
-    value = val
-    isResolved = true
+    if (resolveCallback) {
+      resolveCallback(val)
+    }
+    else {
+      value = val
+      state = 'resolved'
+    }
   }
 
   function reject (val) {
-    value = val
-    isRejected = true
+    if (rejectCallback) {
+      rejectCallback(val)
+    }
+    else {
+      value = val
+      state = 'rejected'
+    }
+  }
+
+  this.then = function (res, rej) {
+    if (state = 'pending') {
+      resolveCallback = res
+      rejectCallback = rej
+    }
+    else if (state === 'resolved') {
+      res(value)
+    }
+    else {
+      rej(value)
+    }
   }
 
   executor(resolve, reject)
-
-  this.then = function thenFunc (resolveCallback, rejectCallback) {
-    if (isResolved) {
-      resolveCallback(value)
-    }
-    else if (isRejected) {
-      rejectCallback(value)
-    }
-    else {
-      setTimeout(thenFunc.bind(null, resolveCallback, rejectCallback), 500)
-    }
-  }
 }
 
 var promise = new MyPromise( function (resolve, reject) {
   var val = Math.random()
   console.log(val);
+  
   if (val > 0.5) {
     setTimeout( function() {
       resolve("Resolved:" + val)
-    }, 1000)
+    }, Math.random() * 1000)
   }
   else {
     setTimeout( function() {
       reject("Rejected:" + val)
-    }, 1000)
+    }, Math.random() * 1000)
   }
 })
 
